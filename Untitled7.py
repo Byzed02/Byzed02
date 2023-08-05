f13b8b478314d07947181fa74e235282c4eef5d0
Byzed02 <136396549+Byzed02@users.noreply.github.com>
2023-08-05 00:23:22 +0000

    Update Untitled7.py

--- Untitled7.py
+++ Untitled7.py
@@ -1,4 +1,4 @@
-{,
+{
       "source": [
         "total_points = st.slider(\"Number of points in spiral\", 1, 5000, 2000)\n",
         "num_turns = st.slider(\"Number of turns in spiral\", 1, 100, 9)\n",
