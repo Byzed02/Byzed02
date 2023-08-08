10ed397c78dc61c2a322e89b3844a8edc8482f73
Byzed02 <136396549+Byzed02@users.noreply.github.com>
2023-08-08 23:33:33 +0000

    Update My_Movies_Recommander.py

--- My_Movies_Recommander.py
+++ My_Movies_Recommander.py
@@ -1,16 +1,6 @@
 {
   "cells": [
     {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/Byzed02/Byzed02/blob/main/My_Movies_Recommander.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
       "cell_type": "code",
       "execution_count": null,
       "id": "7503d809",
